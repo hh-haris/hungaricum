ebc7caf491048819842556813dfab13ea01a72c7
Haris Habib <harishabib.me@gmail.com>
2025-07-22 18:18:30 +0500

    Updated Timeline.tsx

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -187,7 +187,7 @@ const TimelinePage = () => {
             </div>
 
             {/* Timeline */}
-            <div className="mt-23">
+            <div className="mt-123">
               <Timeline defaultValue={3}>
                 {timelineItems.map((item) => (
                   <TimelineItem key={item.id} step={item.id}>
