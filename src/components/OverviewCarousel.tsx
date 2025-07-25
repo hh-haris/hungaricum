035767a8458d7a62b758242a52be5aa8c7fbf42e
Haris Habib <harishabib.me@gmail.com>
2025-07-25 11:16:25 +0500

    Updated OverviewCarousel.tsx

--- src/components/OverviewCarousel.tsx
+++ src/components/OverviewCarousel.tsx
@@ -217,7 +217,7 @@ const OverviewCarousel = () => {
 <div
   className={`flex justify-center space-x-2 transition-all duration-300 ${
     current === 1 || current === 2
-      ? 'mt-[-135px] mb-[-2000px]' // push them up by ~5 inches, add bottom space equal to ~5 inches
+      ? 'mt-[-135px] mb-[2px]' // push them up by ~5 inches, add bottom space equal to ~5 inches
       : 'mt-6 mb-1'             // default for 3rd slide
   }`}
 >
