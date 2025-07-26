75e4f93a4830c9746643b03e410e7fe00cc66cc7
Haris Habib <harishabib.me@gmail.com>
2025-07-26 18:49:22 +0500

    Updated USAT.tsx

--- src/pages/USAT.tsx
+++ src/pages/USAT.tsx
@@ -61,7 +61,7 @@ const USAT = () => {
   const usatCategories = [
     {
       title: "USAT Engineering",
-      image: "0.jpg",
+      image: "03.jpg",
       link: "/usat/engineering"
     },
     {
