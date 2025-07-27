ff5482c3494df954f4bf7caaf5e80b84e2c02d8f
Haris Habib <harishabib.me@gmail.com>
2025-07-27 15:44:04 +0500

    Updated USATArts.tsx

--- src/pages/USATArts.tsx
+++ src/pages/USATArts.tsx
@@ -287,7 +287,7 @@ const USATArts = () => {
                 <Card className="cursor-pointer hover:shadow-lg smooth-transition" onClick={() => setShowStudyResources(true)}>
                   <CardContent className="p-0">
                     <div className="relative w-full h-32 rounded-lg overflow-hidden">
-                      <img src="Other.jpg" alt="Study Resources" className="w-full h-full object-cover" />
+                    <img src="/Other.jpg" alt="Study Resources" className="w-full h-full object-cover" />
                     </div>
                   </CardContent>
                 </Card>
