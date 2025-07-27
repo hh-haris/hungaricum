b476a417c936596e9e8fb85b4fccc5d291d4fc10
Haris Habib <harishabib.me@gmail.com>
2025-07-27 15:44:30 +0500

    Updated USATArts.tsx

--- src/pages/USATArts.tsx
+++ src/pages/USATArts.tsx
@@ -279,8 +279,8 @@ const USATArts = () => {
                 <Card className="cursor-pointer hover:shadow-lg smooth-transition" onClick={() => setShowPastPapers(true)}>
                   <CardContent className="p-0">
                     <div className="relative w-full h-32 rounded-lg overflow-hidden">
-                      <img src="Past Papers.jpg" alt="Past Papers" className="w-full h-full object-cover" />
-                    </div>
+                     <img src="/Past Papers.jpg" alt="Past Papers" className="w-full h-full object-cover" />
+                  </div>
                   </CardContent>
                 </Card>
 
