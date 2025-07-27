de12cc5a5c7cc61c42f731784fdbb79c555a538d
Haris Habib <harishabib.me@gmail.com>
2025-07-27 18:15:30 +0500

    Updated USAT.tsx

--- src/pages/USAT.tsx
+++ src/pages/USAT.tsx
@@ -5,6 +5,7 @@ import { ChevronRight, ChevronUp, X, AlertTriangle } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import { Card, CardContent } from '../components/ui/card';
+import { CheckCircle, Clock } from "lucide-react";
 import {
   Carousel,
   CarouselContent,
