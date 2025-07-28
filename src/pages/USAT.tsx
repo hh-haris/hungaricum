7161c3e3e69250654c797f629617c88ff4f6326b
Haris Habib <harishabib.me@gmail.com>
2025-07-28 19:05:34 +0500

    Updated USAT.tsx

--- src/pages/USAT.tsx
+++ src/pages/USAT.tsx
@@ -421,7 +421,7 @@ const USAT = () => {
               <div className="flex items-start space-x-2">
                 <div className="w-2 h-2 bg-brand-orange rounded-full mt-2 flex-shrink-0"></div>
                 <p className="text-sm text-muted-foreground">
-                  You are given a sheet for essay writing that contains three pages (One sheet, front and back counts as two pages). On the first two pages, you will write an argumentative essay and on the third page, a narrative essay. Both essays can be written in either Urdu or English and your choice of language does not affect your marks.
+                  You are given a sheet for essay writing that contains three pages (One sheet, front and back counts as two pages). You will write one argumentative essay on the first two pages and one narrative essay on the third page, each selected from a choice of three topics. Both essays can be written in either Urdu or English and your choice of language does not affect your marks.
 
 
                 </p>
